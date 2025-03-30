732093fccc130cb34f08d3575d9393f0c2e6ac58
Rakesh Bhugra <rakesh@vurbalize.com>
2025-03-30 14:27:40 +0530

    cleaned the code

--- src/experimental/playwright_learn/stealth_mode_playwright.py
+++ src/experimental/playwright_learn/stealth_mode_playwright.py
@@ -1,4 +1,3 @@
-
 from playwright.sync_api import sync_playwright
 from playwright_stealth import stealth_sync
 
@@ -16,11 +15,6 @@ with sync_playwright() as p:
     page.fill("textarea", "Playwright Python tutorial")
     page.keyboard.press("Enter")
     
-    # click on the first result
-    page.click("text=Playwright Python tutorial")
-
     # Keep the browser window open until user input
     input("Press Enter to close the browser...")
-    browser.close()
-    
     browser.close()
