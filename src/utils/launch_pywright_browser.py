a0d3e88455ba3922196458827966342deaad5261
Rakesh Bhugra <rakesh@vurbalize.com>
2025-03-31 12:43:22 +0530

    added support for proxy

--- src/utils/launch_pywright_browser.py
+++ src/utils/launch_pywright_browser.py
@@ -1,11 +1,30 @@
 from playwright.async_api import async_playwright
 from playwright_stealth import stealth_async
+import os
+from urllib.parse import urlparse
 
-async def launch_pywright_browser():
+async def launch_pywright_browser(use_proxy=False):
     playwright = await async_playwright().start()
+    
+    if use_proxy:
+        proxy_url = os.getenv("PROXY_URL")
+        parsed_url = urlparse(proxy_url)
+        proxy_server = f"{parsed_url.scheme}://{parsed_url.netloc}"
+        proxy_username = parsed_url.username
+        proxy_password = parsed_url.password
+        proxy = {
+            "server": proxy_server,
+            "username": proxy_username,
+            "password": proxy_password,
+        }
+    else:
+        proxy = None
+    
+    
     browser = await playwright.chromium.launch(
         headless=False,  # Make browser visible
-        args=['--start-maximized']  # Start with maximized window
+        args=['--start-maximized'],  # Start with maximized window
+        proxy=proxy
     )
     context = await browser.new_context(
         viewport=None,  # Remove viewport size restrictions
