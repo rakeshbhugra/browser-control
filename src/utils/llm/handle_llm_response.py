bc758d048214c4a9faff205317f1c1c632072aea
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-03-31 09:18:58 +0530

    working checkpoint with history manager

--- src/utils/llm/handle_llm_response.py
+++ src/utils/llm/handle_llm_response.py
@@ -1,9 +1,14 @@
 from src.interfaces.llm_response import LLMResponse
+from src.sqlite_manager.manager import sqlite_manager
 
-async def handle_llm_response(llm_response: LLMResponse):
+async def handle_llm_response(llm_response: LLMResponse, user_id: str):
+
+    # get history for this user_id
+    history = await sqlite_manager.get_previous_events(user_id)
+
     if llm_response.function_name:
         function_name = llm_response.function_name
         function_args = llm_response.function_args
         # call the function
     else:
-        return llm_response.text
+        pass
