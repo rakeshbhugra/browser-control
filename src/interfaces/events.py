8bae72c6461a9fcff8963459e8a9d9041935d2de
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-03-31 09:03:57 +0530

    renames message to event, it makes more sense to have events

--- src/interfaces/events.py
+++ src/interfaces/events.py
@@ -6,11 +6,15 @@ class FunctionDetails(BaseModel):
     args: dict
     response: str
 
-class Message(BaseModel):
+class Event(BaseModel):
+    '''
+    TODO: we should have session_id for same user's multiple sessions but ok for now
+    '''
+    user_id: str
     role: str
     content: str
     message_type: str
     function_details: FunctionDetails
 
-class Messages(BaseModel):
-    messages: List[Message]
+class Events(BaseModel):
+    events: List[Event]
