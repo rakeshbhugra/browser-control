9fb2d7a1df9cb8699a40daa25c6282a1a67fd8bd
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-03-31 10:09:22 +0530

    working checkpoint for handle llm response

--- src/utils/llm/handle_llm_response.py
+++ src/utils/llm/handle_llm_response.py
@@ -2,17 +2,41 @@ from src.interfaces.llm_response import LLMResponse
 from src.sqlite_manager.manager import sqlite_manager
 from src.browser_agent.tools.manager import tool_funcs_manager
 from src.interfaces.tool_response import ToolResponse
+from src.interfaces.events import Event, FunctionDetails
+from playwright.sync_api import Page
+from datetime import datetime
 
-async def handle_llm_response(llm_response: LLMResponse, user_id: str):
-
-    # get history for this user_id
-    history = await sqlite_manager.get_previous_events(user_id)
+async def handle_llm_response(llm_response: LLMResponse, user_id: str, page: Page):
 
     if llm_response.function_name:
         function_name = llm_response.function_name
         function_args = llm_response.function_args
         # call the function
-        tool_response: ToolResponse = await tool_funcs_manager.tool_call(function_name, function_args)
+        tool_response: ToolResponse = await tool_funcs_manager.tool_call(function_name, function_args, page)
         print(tool_response)
+        
+        function_details = FunctionDetails(
+            name=function_name,
+            args=function_args,
+            response=tool_response.text_response
+        )
+        
+        event = Event(
+            user_id=user_id,
+            role="assistant",
+            content=tool_response.text_response,
+            message_type="function_call",
+            function_details=function_details,
+            event_timestamp=datetime.now()
+        )
     else:
-        pass
+        event = Event(
+            user_id=user_id,
+            role="assistant",
+            content=llm_response.text_response,
+            message_type="text",
+            event_timestamp=datetime.now()
+        )
+
+    # save the event
+    await sqlite_manager.create_event(event)
