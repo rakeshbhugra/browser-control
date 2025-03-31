3022e3fee68ab0026f952a071e7240962aa9c335
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-03-31 10:24:38 +0530

    exiting program on task complete

--- src/browser_agent/tools/tool_funcs/task_complete_tool.py
+++ src/browser_agent/tools/tool_funcs/task_complete_tool.py
@@ -1,7 +1,10 @@
 from src.interfaces.tool_response import ToolResponse
+from src.utils.print_helper import print_helper
 
 async def task_complete_tool() -> ToolResponse:
     # Signal that the task is complete
-    print("Task marked as complete")
+    print_helper.green_print("Task marked as complete")
     message = "Task has been marked as complete."
+    # exit the program
+    exit()
     return ToolResponse(text_response=message, tool_output=message) 
