5d7df525a2254517886f8ae1b6ff7db692b6ecbd
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-03-31 20:41:54 +0530

    still wip message management

--- src/utils/prepare_user_prompt.py
+++ src/utils/prepare_user_prompt.py
@@ -1,10 +1,10 @@
 from src.sqlite_manager.manager import sqlite_manager
 from src.interfaces.events import Event
 from typing import List
-
-async def prepare_user_prompt(user_input: str, user_id: str) -> str:
+from src.interfaces.context import Context
+async def prepare_user_prompt(user_input: str, context: Context) -> str:
     # get history for this user_id
-    history: List[Event] = await sqlite_manager.get_previous_events(user_id)
+    history: List[Event] = await sqlite_manager.get_previous_events(context.user_id)
 
     # prepare the user prompt
     user_prompt = ""
