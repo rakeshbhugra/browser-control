09aa175bab22b05685e23f43f54dcdfa4bdae03f
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-04-01 14:54:59 +0530

    working google search checkpoint

--- src/browser_agent/tools/tool_funcs/google_search_tool.py
+++ src/browser_agent/tools/tool_funcs/google_search_tool.py
@@ -1,38 +1,30 @@
 from src.interfaces.tool_response import ToolResponse
 from src.interfaces.context import Context
-from crawl4ai import AsyncWebCrawler
 import os
-async def crawl_raw_html(raw_html: str):
-    raw_html_url = f"raw:{raw_html}"
-    
-    async with AsyncWebCrawler() as crawler:
-        result = await crawler.arun(url=raw_html_url)
-        if result.success:
-            return result.markdown
-        else:
-            return None
+import json
+from src.utils.draw_bounding_boxes_around_elements import draw_bounding_boxes_around_elements, DrawBoundingBoxesAroundElementsResponse
+import traceback
 
 async def google_search_tool(query: str, context: Context) -> ToolResponse:
-    # Implement Google search functionality
-    # This would typically use a search API or web scraping
-    print(f"Searching Google for: {query}")
-    
-    await context.page.goto("https://www.bing.com")
-    await context.page.wait_for_timeout(1000)
-    await context.page.fill("textarea", query)
-    await context.page.keyboard.press("Enter")
-    await context.page.wait_for_timeout(10000)
+    try:
+        print(f"Searching Google for: {query}")
+        
+        await context.page.goto("https://www.bing.com")
+        await context.page.wait_for_timeout(1000)
+        await context.page.fill("textarea", query)
+        await context.page.keyboard.press("Enter")
+        await context.page.wait_for_timeout(1000)
 
-    # wait for page to load
-    await context.page.wait_for_selector("body")
+        return ToolResponse(
+            text_response = f"google searched for {query}, now on search results page",
+            tool_output = "",
+            tool_worked = True
+        )
     
-    # read page html
-    raw_html = await context.page.content()
-    ignored_dir = 'data/gitignored'
-    with open(os.path.join(ignored_dir, "raw.html"), "w") as f:
-        f.write(raw_html)
-    search_markdown = await crawl_raw_html(raw_html)
-    with open(os.path.join(ignored_dir, "search_results.md"), 'w') as f:
-        f.write(search_markdown)
-
-    return ToolResponse(text_response=search_markdown, tool_output=search_markdown)
+    except Exception as e:
+        return ToolResponse(
+            text_response = f'{traceback.format_exc()}',
+            tool_output = "",
+            tool_worked = False
+        )
+        
