1584573ca8764125b2d013b6ac2fcb15f603210b
Rakesh Bhugra <rakesh@vurbalize.com>
2025-04-02 00:41:12 +0530

    fixed task complete

--- src/browser_agent/tools/tool_funcs/task_complete_tool.py
+++ src/browser_agent/tools/tool_funcs/task_complete_tool.py
@@ -1,7 +1,8 @@
 from src.interfaces.tool_response import ToolResponse
 from src.utils.print_helper import print_helper
+from src.interfaces.context import Context
 
-async def task_complete_tool() -> ToolResponse:
+async def task_complete_tool(context: Context) -> ToolResponse:
     # Signal that the task is complete
     print_helper.green_print("Task marked as complete")
     message = "Task has been marked as complete."
