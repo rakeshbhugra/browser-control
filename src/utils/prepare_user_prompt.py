74fe7df769a2082b94f1e6a2554bfc665a3942ae
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-04-01 14:57:29 +0530

    better formatting for user prompt

--- src/utils/prepare_user_prompt.py
+++ src/utils/prepare_user_prompt.py
@@ -2,13 +2,18 @@ from src.sqlite_manager.manager import sqlite_manager
 from src.interfaces.events import Event
 from typing import List
 from src.interfaces.context import Context
-async def prepare_user_prompt(user_input: str, context: Context) -> str:
+import os
+async def prepare_user_prompt(context: Context) -> str:
     # get history for this user_id
     history: List[Event] = await sqlite_manager.get_previous_events(context.user_id)
 
     # prepare the user prompt
-    user_prompt = ""
+    user_prompt = "<conversation>\n"
     for event in history:
         user_prompt += f"{event.role}: {event.content}\n"
-    user_prompt += f"User: {user_input}\n"
+    user_prompt += "</conversation>\n"
+    
+    with open(os.path.join("data/gitignored", "user_prompt.txt"), 'w') as f:
+        f.write(user_prompt)
+    
     return user_prompt
