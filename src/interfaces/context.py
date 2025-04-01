cfb49aff32cf92be8bbc9fa3efeb1788028f4e1c
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-04-01 14:54:47 +0530

    arbitrary_types_allowed to handle Page type

--- src/interfaces/context.py
+++ src/interfaces/context.py
@@ -3,8 +3,13 @@ from playwright.async_api import Page
 from typing import Optional, Dict
 
 class Context(BaseModel):
+    model_config = {
+        "arbitrary_types_allowed": True
+    }
+    
     user_id: str
     page: Optional[Page] = None
+    page_content: Optional[str] = None
     element_index_dict: Optional[Dict] = None
     messages_for_llm: str
     plan: Optional[str] = None
